cca6f1f92a6bd69eb4b88a32d94d438b457a1cae
S.A.A.J <S.A.A.J@DESKTOP-CE4NVTQ>
2020-02-22 11:50:06 +0300

    codd2

---
+++ src/p2/Calculator.java
@@ -0,0 +1,10 @@
+package p2;
+
+public class Calculator {
+
+	public int sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
---
+++ src/p2/yy.java
@@ -0,0 +1,12 @@
+package p2;
+
+import org.junit.Test;
+
+public class yy {
+
+	@Test
+	public void mytest() {
+		System.out.print("Hi guys this is my first test");
+	}	
+	
+}
