67cdd349dbf7f832a5e6df04c35996a06da1ccf6
S.A.A.J <S.A.A.J@DESKTOP-CE4NVTQ>
2020-02-26 02:53:12 +0300

    up2

---
+++ src/p2/AllTests.java
@@ -0,0 +1,11 @@
+package p2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Calculater.class, yy.class })
+public class AllTests {
+
+}
--- src/p2/yy.java
+++ src/p2/yy.java
@@ -1,12 +1,24 @@
 package p2;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
 public class yy {
 
 	@Test
-	public void mytest() {
-		System.out.print("Hi guys this is my first test");
-	}	
+	public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber() {  
+		// Arrange  
+		int a = 10;     int b = 20;  
+		Calculator calc = new Calculator(); 
+	   // Act    
+	     int result = calc.sum(a, b); 
+	   // Assert   
+	   assertTrue(result > 0); 
+	    } 
+	 
 	
 }
