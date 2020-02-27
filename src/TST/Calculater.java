1fe2749637ffd630b9148113c15b23c4971c421e
S.A.A.J <S.A.A.J@DESKTOP-CE4NVTQ>
2020-02-27 22:57:36 +0300

    BBB

--- src/TST/Calculater.java
+++ src/TST/Calculater.java
@@ -1,5 +1,6 @@
 package TST;
 
+
 import static org.junit.Assert.*;
 
 import org.junit.After;
--- src/p2/Calculator.java
+++ src/p2/Calculator.java
@@ -2,6 +2,7 @@ package p2;
 
 public class Calculator {
 
+	
 	public int sum(int a, int b) {
 		// TODO Auto-generated method stub
 		return a+b;
